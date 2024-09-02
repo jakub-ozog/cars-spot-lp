5e2522298f5571a028bc2da142c088d7d3552c96
jakub-ozog <jakubjanozog@gmail.com>
2024-09-02 23:49:15 +0200

    feat(header): add header structure and custom style attributes to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./build/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background-main': '#f7f7f7',
+        'accent-blue': '#0147ff'
+      },
+      maxWidth: {
+        'container-main': '1440px'
+      }, 
+      fontFamily: {
+        'roboto-condensed': ['Roboto Condensed', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
