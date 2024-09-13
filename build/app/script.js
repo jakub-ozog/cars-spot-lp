f06b12bc04d7441af44cd2f06ed4a482d90d40d1
jakub-ozog <jakubjanozog@gmail.com>
2024-09-13 11:17:01 +0200

    feat(mobile-menu) mobile menu structure update

--- build/app/script.js
+++ build/app/script.js
@@ -4,26 +4,36 @@ const mobileBtnCls = document.querySelector('.mobile-btn-cls')
 const mobileMenu = document.querySelector('.mobile-menu')
 const heroSection = document.getElementById('#hero')
 const body = document.querySelector('body')
+const mobileLinks = mobileMenu.querySelectorAll('li')
 
-    
 
 mobileBtn.addEventListener('click', () => {
     mobileMenu.classList.toggle('hidden')
     mobileMenu.classList.toggle('grid')
     heroSection.classList.toggle('hidden')
-    mobileBtnCls.classList.remove('hidden');  
+    mobileBtnCls.classList.remove('hidden');
     body.classList.add('body-no-scroll')
-
 })
 
 mobileBtnCls.addEventListener('click', () => {
     mobileMenu.classList.remove('open', 'grid');
     mobileMenu.classList.toggle('hidden')
-    mobileBtnCls.classList.toggle('hidden'); 
+    mobileBtnCls.classList.toggle('hidden');
     heroSection.classList.toggle('hidden')
     body.classList.remove('body-no-scroll')
-    
 });
+
+
+
+mobileLinks.forEach(item => {
+    item.addEventListener('click', () => {
+        mobileMenu.classList.remove('open', 'grid');
+        mobileMenu.classList.toggle('hidden')
+        mobileBtnCls.classList.toggle('hidden');
+        heroSection.classList.toggle('hidden')
+        body.classList.remove('body-no-scroll')
+    })
+})
 
 expandBtn.forEach(button => {
     button.addEventListener('click', () => {
