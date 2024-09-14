2ddf902c484834eeef06dc2eef52d751a4afcd65
jakub-ozog <jakubjanozog@gmail.com>
2024-09-14 15:37:33 +0200

    chore: responsive design update

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,17 +9,21 @@ module.exports = {
         'black-main': '#282828'
       },
       maxWidth: {
-        'container-main': '1440px'
+        'container-main': '1440px',
+      },
+      screens: {
+        'xl-screen': '1920px',
+        'xxl-screen': '2560px'
       },
       fontFamily: {
         'roboto-condensed': ['Roboto Condensed', 'sans-serif'],
         'roboto-flex': ['Roboto Flex', 'sans-serif'],
         'bebas-neue': ['Bebas Neue', 'sans-serif']
-      }, 
+      },
       fontSize: {
         'font-cta': '15px',
         'font-seo': '25px'
-      }, 
+      },
     },
   },
   plugins: [],
